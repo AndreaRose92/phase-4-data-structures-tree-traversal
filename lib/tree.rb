7a3dd43e86883bbbca7f5285755fe5ae0d7c75c9
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-31 13:12:18 -0700

    commit tree 0a9aafd9c4bda0bd8b5523f9e25ce04756d20574

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,34 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def breadth_first_traversal node
+    result = []
+    nodes_to_visit = [node]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node[:value])
+      nodes_to_visit = nodes_to_visit + node[:children]
+    end
+
+    result
+  end
+
+  def depth_first_traversal node
+    result = []
+    nodes_to_visit = [node]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node[:value])
+      nodes_to_visit = node[:children] + nodes_to_visit
+    end
+
+  end
+
+  def get_element_by_id node
+
+  end
+
 end
